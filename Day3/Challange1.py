81d74eda5edf37e2930c935f085b8cacb4f66e9d
Mark van Dooremaal <vandooremaal@gmail.com>
2021-12-07 20:11:06 +0100

    Update Challange1.py

--- Day3/Challange1.py
+++ Day3/Challange1.py
@@ -3,15 +3,15 @@ from collections import Counter
 with open('/Users/mark/Projects/AoC2021/Day3/input.txt') as f:
     data = f.read().splitlines()
 
-theta = ''
+gamma = ''
 epsilon = ''
 
 for i in range(len(data[0])):
     common = Counter([x[i] for x in data])
     if common['0'] > common['1']:
-        theta += '0'
+        gamma += '0'
         epsilon += '1'
     else:
-        theta += '1'
+        gamma += '1'
         epsilon += '0'
-print(int(theta, 2) * int(epsilon, 2))
+print(int(gamma, 2) * int(epsilon, 2))
